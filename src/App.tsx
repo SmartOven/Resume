827cea4dd435650519c008a7a8839f5bde254261
smartoven <yarik.panteleev@gmail.com>
2024-01-11 16:46:21 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import React from "react";
+
+const App: React.FC = () => {
+    return (
+        <>
+            <h1>Yaroslav Panteleev</h1>
+            <div>
+                Hi there! Here you can find my CV and some of my pet-projects
+            </div>
+        </>
+    )
+}
+
+export default App
