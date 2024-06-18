114709b232ded92e64176e090e07f35d0d6303f0
smartoven <yarik.panteleev@gmail.com>
2024-06-18 17:58:09 +0300

    fix nginx conf

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,26 @@ const App: React.FC = () => {
         <>
             <h1>Yaroslav Panteleev</h1>
             <div>
-                Hi there! Here you can find my CV and some of my pet-projects
+                Hi there!
             </div>
+            <div>Here you can find my CV and some of my pet-projects</div>
+            <ul>
+                <li><b>Miallo.</b> A straightforward messenger</li>
+                <ul>
+                    <li><a href="http://miallo.panteleevya.ru">Try it here</a></li>
+                    <li><a href="https://github.com/SmartOven/Miallo">GitHub repository</a></li>
+                </ul>
+                <li><b>The phone store.</b> An e-commerce website for selling phones</li>
+                <ul>
+                    <li><a href="http://thephonestore.panteleevya.ru">Try it here</a></li>
+                    <li><a href="https://github.com/SmartOven/ThePhoneStore">GitHub repository</a></li>
+                </ul>
+                <li><b>Paste.</b> A service for quick and handy sharing of formatted texts</li>
+                <ul>
+                    <li><a href="http://paste.panteleevya.ru">Try it here</a></li>
+                    <li><a href="https://github.com/SmartOven/paste">GitHub repository</a></li>
+                </ul>
+            </ul>
         </>
     )
 }
